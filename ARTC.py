6ceaceaccb882aa65bc792a189ebf4497bedbaf7
Steven Goldberg <steven.goldberg@emc.com>
2017-11-06 14:17:55 -0500

    Reorganize review parsing - Fix product name and price, add verified purchase Cleanup word cloud generation.  Add data cleanup

--- ARTC.py
+++ ARTC.py
@@ -15,9 +15,40 @@ def FindAllReviews(asin):
 
 	XPATH_ALL_REVIEWS = '//a[@id="dp-summary-see-all-reviews"]//@href'
 	all_reviews = parser.xpath(XPATH_ALL_REVIEWS)
-	return(ParseReviews("http://www.amazon.com" + all_reviews[0]))
 
-def ParseReviews(review_url):
+	XPATH_AGGREGATE_RATING = '//table[@id="histogramTable"]//tr'
+	XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
+	XPATH_PRODUCT_PRICE  = '//span[@id="priceblock_ourprice"]/text()'
+	
+	raw_product_price = parser.xpath(XPATH_PRODUCT_PRICE)
+	product_price = ''.join(raw_product_price).replace(',','')
+	raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
+	product_name = ''.join(raw_product_name).strip()
+
+	reviews_list = ParseReviews("http://www.amazon.com" + all_reviews[0], 0)
+
+	total_ratings  = parser.xpath(XPATH_AGGREGATE_RATING)
+	ratings_dict = {}
+	#grabing the rating  section in product page
+	for ratings in total_ratings:
+		extracted_rating = ratings.xpath('./td//a//text()')
+		if extracted_rating:
+			rating_key = extracted_rating[0] 
+			raw_raing_value = extracted_rating[1]
+			rating_value = raw_raing_value
+			if rating_key:
+				ratings_dict.update({rating_key:rating_value})
+
+	data = {
+			'ratings':ratings_dict,
+			'reviews':reviews_list,
+			'url':amazon_url,
+			'price':product_price,
+			'name':product_name
+		}
+	return(data)
+
+def ParseReviews(review_url, iter):
 	# Added Retrying 
 	print("attempting: " + review_url)
 	for i in range(5):
@@ -30,32 +61,12 @@ def ParseReviews(review_url):
 			XPATH_AGGREGATE = '//span[@id="acrCustomerReviewText"]'
 			XPATH_REVIEW_SECTION_1 = '//div[@data-hook="review"]'
 
-			XPATH_AGGREGATE_RATING = '//table[@id="histogramTable"]//tr'
-			XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
-			XPATH_PRODUCT_PRICE  = '//span[@id="priceblock_ourprice"]/text()'
-			
-			raw_product_price = parser.xpath(XPATH_PRODUCT_PRICE)
-			product_price = ''.join(raw_product_price).replace(',','')
-
-			raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
-			product_name = ''.join(raw_product_name).strip()
-			total_ratings  = parser.xpath(XPATH_AGGREGATE_RATING)
 			reviews = parser.xpath(XPATH_REVIEW_SECTION_1)
-			ratings_dict = {}
 			reviews_list = []
 			
 			if not reviews:
 				raise ValueError('unable to find reviews in page')
 
-			#grabing the rating  section in product page
-			for ratings in total_ratings:
-				extracted_rating = ratings.xpath('./td//a//text()')
-				if extracted_rating:
-					rating_key = extracted_rating[0] 
-					raw_raing_value = extracted_rating[1]
-					rating_value = raw_raing_value
-					if rating_key:
-						ratings_dict.update({rating_key:rating_value})
 			#Parsing individual reviews
 			for review in reviews:
 				XPATH_RATING  = './/i[@data-hook="review-star-rating"]//text()'
@@ -65,8 +76,10 @@ def ParseReviews(review_url):
 				XPATH_REVIEW_TEXT_1 = './/span[@data-hook="review-body"]//text()'
 				XPATH_REVIEW_TEXT_2 = './/div//span[@data-action="columnbalancing-showfullreview"]/@data-columnbalancing-showfullreview'
 				XPATH_REVIEW_COMMENTS = './/span[@data-hook="review-comment"]//text()'
-				XPATH_AUTHOR  = './/a[contains(@href,"/profile/")]/parent::span//text()'
+				#XPATH_AUTHOR  = './/a[contains(@href,"/profile/")]/parent::span//text()'
+				XPATH_AUTHOR = './/a[@data-hook="review-author"]//text()'
 				XPATH_REVIEW_TEXT_3  = './/div[contains(@id,"dpReviews")]/div/text()'
+				XPATH_REVIEW_VERIFIED = './/span[@data-hook="avp-badge"]//text()'
 				raw_review_author = review.xpath(XPATH_AUTHOR)
 				raw_review_rating = review.xpath(XPATH_RATING)
 				raw_review_header = review.xpath(XPATH_REVIEW_HEADER)
@@ -74,13 +87,13 @@ def ParseReviews(review_url):
 				raw_review_text1 = review.xpath(XPATH_REVIEW_TEXT_1)
 				raw_review_text2 = review.xpath(XPATH_REVIEW_TEXT_2)
 				raw_review_text3 = review.xpath(XPATH_REVIEW_TEXT_3)
+				raw_review_verified = review.xpath(XPATH_REVIEW_VERIFIED)
 
-				#print(raw_review_text1)
-
-				author = ' '.join(' '.join(raw_review_author).split()).strip('By')
+				#print(raw_review_verified)
+				author = ' '.join(' '.join(raw_review_author).split())
 
 				#cleaning data
-				review_rating = ''.join(raw_review_rating).replace('out of 5 stars','')
+				review_rating = ''.join(raw_review_rating).replace(' out of 5 stars','')
 				review_header = ' '.join(' '.join(raw_review_header).split())
 				review_posted_date = dateparser.parse(''.join(raw_review_posted_date)).strftime('%d %b %Y')
 				review_text = ' '.join(' '.join(raw_review_text1).split())
@@ -99,33 +112,36 @@ def ParseReviews(review_url):
 				raw_review_comments = review.xpath(XPATH_REVIEW_COMMENTS)
 				review_comments = ''.join(raw_review_comments)
 				review_comments = re.sub('[A-Za-z]','',review_comments).strip()
+
+				if len(raw_review_verified) > 0:
+					review_verified = True
+				else:
+					review_verified = False
 				review_dict = {
 									#'review_comment_count':review_comments,
 									'review_text':full_review_text,
 									'review_posted_date':review_posted_date,
 									'review_header':review_header,
 									'review_rating':review_rating,
-									'review_author':author
+									'review_author':author,
+									'review_verified_purchaser':review_verified
 
 								}
-				print(review_dict["review_author"] + " " + str(len(review_dict["review_text"])))
+				#print(review_dict["review_author"] + " " + str(len(review_dict["review_text"])))
 				reviews_list.append(review_dict)
 
-			data = {
-						'ratings':ratings_dict,
-						'reviews':reviews_list,
-						#'url':amazon_url,
-						#'price':product_price,
-						#'name':product_name
-					}
 			XPATH_NEXT_REVIEWS = '//li[@class="a-last"]//a/@href'
 			all_reviews = parser.xpath(XPATH_NEXT_REVIEWS)
+			#stop early if testing
+			#if iter == 1:
+				#return reviews_list
 			if all_reviews:
-				new_data = ParseReviews("http://www.amazon.com" + all_reviews[0])
+				new_data = ParseReviews("http://www.amazon.com" + all_reviews[0], iter+1)
 				if (new_data != None):
-					data["reviews"] = data["reviews"] + new_data["reviews"]
-				return data
-			return None
+					reviews_list += new_data
+				return reviews_list
+			else:
+				return None
 		except ValueError:
 			print("Retrying to get the correct response")
 	
